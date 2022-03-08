cb666afd5683fcafc2164e7e8673fb46d75bca1d
Thomas Lundstrom <thomaswlundstrom@gmail.com>
2022-03-08 10:56:52 -0800

    added functionality to get and create people

---
+++ client/src/components/PersonForm.js
@@ -0,0 +1,38 @@
+import react, {useEffect, useState} from 'react'
+import axios from 'axios';
+
+const PersonForm = () => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        axios.post('http://localhost:8000/api/people',{
+            firstName,
+            lastName
+        })
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+            })
+            .catch(err => console.log(err));
+    }
+
+    return(
+        <>
+            <form onSubmit={submitHandler}>
+                <label>First Name:</label><br/>
+                <input type="text" onChange={(e) => setFirstName(e.target.value)} /> <br/>
+                <label>Last Name:</label><br/>
+                <input type="text" onChange={(e) => setLastName(e.target.value)}/> <br/>
+                <input type="submit" value="Submit" />
+            </form>
+            <div>
+                
+            </div>
+        </>
+    )
+}
+
+export default PersonForm
